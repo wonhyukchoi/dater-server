21753fd4dabb9ce8cea22319bfe731f44b1fea25
Wonhyuk Choi <wc012013@mymail.pomona.edu>
2022-12-12 18:49:21 -0800

    minimal version

--- app/Main.hs
+++ app/Main.hs
@@ -1,6 +1,39 @@
-import Application () -- for YesodDispatch instance
-import Foundation ( App(App) )
-import Yesod.Core ( warp )
+{-# LANGUAGE OverloadedStrings #-}
+{-# LANGUAGE QuasiQuotes       #-}
+{-# LANGUAGE TemplateHaskell   #-}
+{-# LANGUAGE TypeFamilies      #-}
+{-# LANGUAGE ViewPatterns      #-}
+{-# OPTIONS_GHC -fno-warn-unused-top-binds #-}
+
+import Data.Text
+import Yesod
+import Dater (process)
+
+data App = App
+
+mkYesod "App" [parseRoutes|
+/ HomeR GET
+/dater/#Text DaterR GET
+|]
+
+instance Yesod App
+
+getHomeR :: Handler Html
+getHomeR = defaultLayout $ do
+    setTitle "Minimal Multifile"
+    [whamlet|
+        <p>
+            <a href=@{DaterR "sample"}>HTML addition
+    |]
+
+getDaterR :: Text -> Handler Html
+getDaterR input = defaultLayout $ do
+  setTitle "Dater: A Web Version"
+  processed <- liftIO $ process $ unpack input
+  let result = case processed of
+                 Left err  -> err
+                 Right val -> val
+  [whamlet|#{result}|]
 
 main :: IO ()
 main = warp 3000 App
